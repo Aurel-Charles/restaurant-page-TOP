53385cbdf9c7e338d293fa3769e20da6494e8c53
Aurel-Charles <charlesaurelienpro@gmail.com>
2026-03-10 15:43:10 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import "./style.css"
 import { homePageload } from "./home-page.js";
 import { menuPageLoad } from "./menu.js";
 import { aboutPageLoad } from "./about.js";
+import { activeButton, handleBtnListener } from "./utilities.js";
 
-const btnHome  = document.querySelector('#btn-home')
-const btnMenu  = document.querySelector('#btn-menu')
-const btnAbout  = document.querySelector('#btn-about')
-
-const container = document.querySelector('#content')
+const btnHome = document.querySelector('#btn-home')
 
 homePageload()
+activeButton(btnHome)
+handleBtnListener(homePageload,menuPageLoad,aboutPageLoad)
 
-btnHome.addEventListener('click', ()=>{
-    container.replaceChildren()
-    homePageload()
-})
-btnMenu.addEventListener('click', ()=>{
-    container.replaceChildren()
-    menuPageLoad()
-})
-btnAbout.addEventListener('click', ()=>{
-    container.replaceChildren()
-    aboutPageLoad()
-})
+
--- src/utilities.js
+++ src/utilities.js
@@ -1,3 +1,7 @@
+import { aboutPageLoad } from "./about.js"
+import { homePageload } from "./home-page.js"
+import { menuPageLoad } from "./menu.js"
+
 // ./src/utilities.js
 function imageMaker(source, description) {
     const img = document.createElement('img')
@@ -26,5 +30,52 @@ function heroMaker(imageSrc,imageDescription, titleText) {
     return heroWrapper
 }
 
+function activeButton(activeButton) {
+    const btnHome  = document.querySelector('#btn-home')
+    const btnMenu  = document.querySelector('#btn-menu')
+    const btnAbout  = document.querySelector('#btn-about')
+    
+    btnHome.classList.remove("active")
+    btnMenu.classList.remove("active")
+    btnAbout.classList.remove("active")
+    
 
-export { heroMaker , imageMaker, titleMaker}
+    activeButton.classList.add("active")
+}
+
+function clearContainer() {
+    const container = document.querySelector('#content')
+    container.replaceChildren()
+}
+
+function handleBtnListener(homePageload, menuPageLoad, aboutPageLoad) {
+    const btnCliked = document.querySelectorAll('button')
+    const btnHome  = document.querySelector('#btn-home')
+    const btnMenu  = document.querySelector('#btn-menu')
+    const btnAbout  = document.querySelector('#btn-about')
+
+    btnCliked.forEach(element => {
+        element.addEventListener('click', (e)=>{
+            let idButton = e.target.id
+            if (idButton == "btn-home") {
+                clearContainer()
+                homePageload()
+                activeButton(btnHome)
+            }
+            else if (idButton == "btn-menu") {
+                clearContainer()
+                menuPageLoad()
+                activeButton(btnMenu)
+            }
+            else if (idButton == "btn-about") {
+                clearContainer()
+                aboutPageLoad()
+                activeButton(btnAbout)
+            }
+            console.log(e.target.id);
+            
+        })
+    });
+}
+
+export { heroMaker , imageMaker, titleMaker, activeButton, handleBtnListener}
