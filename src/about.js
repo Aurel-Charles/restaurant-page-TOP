c5c20b8f3de793bfd0e97fbeb3bb6b0c161a3088
Aurel-Charles <charlesaurelienpro@gmail.com>
2026-03-10 11:20:06 +0100

    add style and logic for date higlight

--- src/about.js
+++ src/about.js
@@ -1,4 +1,12 @@
+import heroImageScr from "./img/about.jpg"
 import { info, hours } from "./restaurant-data.js";
+import { heroMaker } from "./utilities.js";
+import { format } from "date-fns";
+
+function renderHero(container) {
+    const hero =  heroMaker(heroImageScr, "Bar view", "Come Auround !")
+    container.appendChild(hero)
+}
 
 function renderAddress(container) {
     const infoDiv = document.createElement('div')
@@ -28,12 +36,17 @@ function renderAddress(container) {
 }
 
 function renderHours(container) {
+    let today = format(new Date(), "eeee")
+    today = today.toLowerCase()
+
     const hoursDiv = document.createElement('div')
     hoursDiv.classList.add('hours-container')
 
     hours.forEach(element => {
         const dayDiv = document.createElement('div')
-
+        if (today === element.day.toLowerCase()) {
+            dayDiv.classList.add('today')
+        }
         const day = document.createElement('p')
         day.textContent = element.day
         const time = document.createElement('p')
@@ -50,6 +63,8 @@ function renderHours(container) {
 
 export function aboutPageLoad() {
     const container = document.querySelector('#content')
+    renderHero(container)
     renderAddress(container)
     renderHours(container)
 }
+
